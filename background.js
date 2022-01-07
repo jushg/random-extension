b663fec8ed72e2e8eb4fa3a68a8240e649b9b742
Trong Tan <e0550425@u.nus.edu>
2022-01-07 16:06:52 +0800

    PopUp UI

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 let color = '#3aa757';
+let activateColor = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ activateColor });
+  console.log('Default background color set to %cgreen', `color: ${activateColor}`);
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,15 @@
 // Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
+let activateButton = document.getElementById("activateButton")
+
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
+chrome.storage.sync.get("activateColor",({ color }) => {
+  activateButton.style.backgroundColor = color;
+});
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -14,6 +19,18 @@ changeColor.addEventListener("click", async () => {
     function: setPageBackgroundColor,
   });
 });
+
+activateButton.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: activateScript,
+  });
+});
+
+function activateScript() {
+  
+}
 
 // The body of this function will be execuetd as a content script inside the
 // current page
