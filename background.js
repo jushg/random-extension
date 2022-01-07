a37c3137a932f10adf0890ad2c009f63e13dc29c
hungvo0603 <hungvo1011@gmail.com>
2022-01-07 19:24:51 +0800

    Add initial google.storage

--- background.js
+++ background.js
@@ -1,6 +1,38 @@
-let color = '#3aa757';
+console.log("background running");
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+const storageCache = {};
+
+const initStorageCache = getAllStorageSyncData().then(items => {
+  Object.assign(storageCache, items);
 });
+
+chrome.action.onClicked.addListener(async (tab) => {
+  try {
+    await initStorageCache;
+  } catch (e) {
+    // Handle error that occurred during storage initialization.
+  }
+});
+
+// document.getElementById("set").onclick = function() {
+//     var url = document.getElementById("url").value;
+//     var opt = document.getElementById("opt").value;
+//     chrome.storage.sync.set({ [url] : opt }, function() {
+//         if (chrome.runtime.error) {
+//             console.log("Runtime error.");
+//         }
+//         alert("Sync Successfully");
+//     });
+// }
+
+// Reads all data out of storage.sync and exposes it via a promise.
+function getAllStorageSyncData() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(null, (items) => {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      resolve(items);
+    });
+  });
+}
